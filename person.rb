dfc80e6dfa69591b2a6e0e1d8e03706b8820812e
V-Blaze <valentine6586@gmail.com>
2023-01-30 15:15:35 +0100

    Update Person class methods with more readble code syntax

--- person.rb
+++ person.rb
@@ -11,16 +11,12 @@ class Person
   attr_reader :id
 
   def can_use_services?
-    return unless is_of_age? || @parent_permission
-
-    true
+    of_age?() || @parent_permission
   end
 
   private
 
   def of_age?
-    return true if @age >= 18
-
-    false
+    @age >= 18
   end
 end
